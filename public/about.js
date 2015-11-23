af16478caf02939c824202fc8c46e40c917ecdae
Maksym Radko <tttmaximttt@gmail.com>
2015-11-23 22:53:15 +0200

    initial commit

---
+++ public/about.js
@@ -0,0 +1,20 @@
+var about =
+webpackJsonp_name_([0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var _welcome = __webpack_require__(1);
+	
+	var _welcome2 = _interopRequireDefault(_welcome);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	(0, _welcome2.default)('about');
+	
+	exports.welcome = _welcome2.default;
+
+/***/ }
+]);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hYm91dC5lczYiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUVBLHdCQUFRLE9BQU8sQ0FBQyxDQUFDOztBQUVqQixRQUFPLENBQUMsT0FBTyxvQkFBVSxDIiwiZmlsZSI6ImFib3V0LmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHdlY2xvbWUgZnJvbSAnLi93ZWxjb21lJztcblxud2VjbG9tZSgnYWJvdXQnKTtcblxuZXhwb3J0cy53ZWxjb21lID0gd2VjbG9tZTtcblxuXG4vKiogV0VCUEFDSyBGT09URVIgKipcbiAqKiAuL2Fib3V0LmVzNlxuICoqLyJdLCJzb3VyY2VSb290IjoiIn0=
---
+++ public/home.js
@@ -0,0 +1,20 @@
+var home =
+webpackJsonp_name_([2],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var _welcome = __webpack_require__(1);
+	
+	var _welcome2 = _interopRequireDefault(_welcome);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	(0, _welcome2.default)('home');
+	
+	exports.welcome = _welcome2.default;
+
+/***/ }
+]);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9ob21lLmVzNiJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7O0FBRUEsd0JBQVEsTUFBTSxDQUFDLENBQUM7O0FBRWhCLFFBQU8sQ0FBQyxPQUFPLG9CQUFVLEMiLCJmaWxlIjoiaG9tZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB3ZWNsb21lIGZyb20gJy4vd2VsY29tZSc7XG5cbndlY2xvbWUoJ2hvbWUnKTtcblxuZXhwb3J0cy53ZWxjb21lID0gd2VjbG9tZTtcblxuXG4vKiogV0VCUEFDSyBGT09URVIgKipcbiAqKiAuL2hvbWUuZXM2XG4gKiovIl0sInNvdXJjZVJvb3QiOiIifQ==
